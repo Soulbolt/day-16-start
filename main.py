07811b244609b3e79e7f3b261c26e3e000761ef4
Soulbolt <soulbolt@gmail.com>
2023-12-12 11:09:12 -0700

    Modifying Object Attributes and Calling Methods

--- main.py
+++ main.py
@@ -16,3 +16,6 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
