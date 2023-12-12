3d1f8320e831007103114d75c84bc7c730326822
Soulbolt <soulbolt@gmail.com>
2023-12-12 10:51:41 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# from turtle import Turtle, Screen
+# # Initializing/Create an object
+# timmy = Turtle()
+# print(timmy)
+# # Calling methods to change turtles attributes
+# timmy.shape("turtle")
+# timmy.color("purple")
+# # Make the turtle move
+# timmy.forward(100)
+# # Accessing attributes from an object by the . notation
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# # Calling a method/function from an object
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
